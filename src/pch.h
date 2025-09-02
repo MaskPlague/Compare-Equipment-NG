07bf191fb7d49455df655c84b1363c79b3c825ab
MaskPlague <152964338+MaskPlague@users.noreply.github.com>
2025-09-02 08:01:19 -0500

    Initial commit

---
+++ src/pch.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#include "RE/Skyrim.h"
+#include "SKSE/SKSE.h"
+#include <spdlog/sinks/basic_file_sink.h>
+#include <windows.h>
+
+#define DLLEXPORT __declspec(dllexport)
+
+using namespace std::literals;
