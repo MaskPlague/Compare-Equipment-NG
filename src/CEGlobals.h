7a53a8b67100a12f04015cec82ad6a3a934f45eb
MaskPlague <152964338+MaskPlague@users.noreply.github.com>
2025-09-09 06:12:39 -0500

    rename var

--- src/CEGlobals.h
+++ src/CEGlobals.h
@@ -5,12 +5,11 @@ namespace CEGlobals
     extern double X_ORIGIN;
     extern double Y_ORIGIN;
     extern int BACKGROUND_ALPHA;
-    extern bool PERMANENT_OPEN;
     extern uint32_t COMPARE_KEY;
     extern uint32_t CYCLE_KEY;
     extern std::chrono::milliseconds HOLD_THRESHOLD;
     extern std::chrono::milliseconds TRIPLE_HIT_WINDOW;
-    extern std::chrono::milliseconds GET_SETTINGS_THRESHOLD;
+    extern std::chrono::milliseconds SETTING_HOLD_THRESHOLD;
     extern int LOG_LEVEL;
 
     const int EQUIPPED_ITEM_ARRAY_SIZE = 6;
