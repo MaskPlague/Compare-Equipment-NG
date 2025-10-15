f38b1dae885177e136fb04abeb43b4dc1abd11b0
MaskPlague <152964338+MaskPlague@users.noreply.github.com>
2025-10-15 01:27:37 -0500

    update version

--- src/CEIconUtils.h
+++ src/CEIconUtils.h
@@ -3,6 +3,7 @@
 namespace CEIconUtils
 {
     // std::string GetArmorIconLabel(RE::TESObjectARMO *armor);
+
     RE::GFxValue GetSelectedEntryObject();
 
     RE::GFxValue GetEquippedEntryObject(RE::FormID equippedFormID);
