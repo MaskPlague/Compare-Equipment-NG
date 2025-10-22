0284225515d676a73447d9a7de63a1cb21a30f53
MaskPlague <152964338+MaskPlague@users.noreply.github.com>
2025-10-22 04:08:59 -0500

    remove unused functions

--- src/CEIconUtils.h
+++ src/CEIconUtils.h
@@ -2,8 +2,6 @@
 
 namespace CEIconUtils
 {
-    // std::string GetArmorIconLabel(RE::TESObjectARMO *armor);
-
     RE::GFxValue GetSelectedEntryObject();
 
     RE::GFxValue GetEquippedEntryObject(RE::FormID equippedFormID);
